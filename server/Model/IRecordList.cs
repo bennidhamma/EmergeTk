2352c98011234536f3f0a95b3af769434b56aa99
Benjamin Joldersma <ben@forgottenarts.com>
2013-02-25 21:39:37 -0800

    remove ambigious removeall method

--- server/Model/IRecordList.cs
+++ server/Model/IRecordList.cs
@@ -26,60 +26,99 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * Author: Ben Joldersma
  *   
  */
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 namespace EmergeTk.Model
 {
-    public interface IRecordList : IComparable, IJSONSerializable, IEnumerable
-    {
-        //properties
-        int Count { get; }
-        List<SortInfo> Sorts { get; set; }
-        List<FilterInfo> Filters { get; set; }
-        AbstractRecord this[int index] { get; set; }
-        Type RecordType { get; set; }
-        bool Preserve { get; set; }
+	public interface IRecordList : IComparable, IJSONSerializable, IEnumerable
+	{
+		//properties
+		int Count { get; }
+
+		List<SortInfo> Sorts { get; set; }
+
+		List<FilterInfo> Filters { get; set; }
+
+		AbstractRecord this [int index] { get; set;
+		}
+
+		Type RecordType { get; set; }
+
+		bool Preserve { get; set; }
+
 		AbstractRecord Parent { get; set; }
-		List<int> RecordSnapshot { get; set;	}
+
+		List<int> RecordSnapshot { get; set; }
         
-        //methods
-        void Add(AbstractRecord value);
-        void AddFilter(FilterInfo filterInfo);
-        void Clear();
-        bool Contains(AbstractRecord value);
-        void Delete();
-        void Delete(AbstractRecord value);
-        void DeleteAt(int index);
-        IRecordList Filter();
-        IRecordList Filter(Predicate<AbstractRecord> record);
-        IRecordList Filter(params FilterInfo[] filters);
-       	//IEnumerator GetEnumerator();
-		IEnumerable<AbstractRecord> GetEnumerable();
-        int IndexOf(AbstractRecord value);
-        void Insert(int index, AbstractRecord value);
-        bool IsReadOnly { get; }
-        AbstractRecord NewRow<T>() where T : AbstractRecord, new();
-        void Remove(AbstractRecord value);
-        void RemoveAt(int index);
-		void RemoveRange( int index, int count );
-		void RemoveAll (Predicate<AbstractRecord> match);
-        void Save();
-        void Sort();
-		void Randomize();
-        void Sort(params SortInfo[] sorts);
-        void Sort( Comparison<AbstractRecord> comparison );
-        void ForEach( Action<AbstractRecord> action );
-        string[] ToIdArray();
-        U[] GetVector<U>(string column);
-        new Dictionary<string,object> Serialize();
-        new void Deserialize(Dictionary<string,object> h);
-        string[] ToStringArray();
-        string[] ToStringArray(string property);
-        AbstractRecord[] ToArray();
-        IRecordList Copy();
-		bool TestAny( IRecordList irl );
+		//methods
+		void Add (AbstractRecord value);
+
+		void AddFilter (FilterInfo filterInfo);
+
+		void Clear ();
+
+		bool Contains (AbstractRecord value);
+
+		void Delete ();
+
+		void Delete (AbstractRecord value);
+
+		void DeleteAt (int index);
+
+		IRecordList Filter ();
+
+		IRecordList Filter (Predicate<AbstractRecord> record);
+
+		IRecordList Filter (params FilterInfo[] filters);
+		//IEnumerator GetEnumerator();
+		IEnumerable<AbstractRecord> GetEnumerable ();
+
+		int IndexOf (AbstractRecord value);
+
+		void Insert (int index, AbstractRecord value);
+
+		bool IsReadOnly { get; }
+
+		AbstractRecord NewRow<T> () where T : AbstractRecord, new();
+
+		void Remove (AbstractRecord value);
+
+		void RemoveAt (int index);
+
+		void RemoveRange (int index, int count);
+
+		void Save ();
+
+		void Sort ();
+
+		void Randomize ();
+
+		void Sort (params SortInfo[] sorts);
+
+		void Sort (Comparison<AbstractRecord> comparison);
+
+		void ForEach (Action<AbstractRecord> action);
+
+		string[] ToIdArray ();
+
+		U[] GetVector<U> (string column);
+
+		new Dictionary<string,object> Serialize ();
+
+		new void Deserialize (Dictionary<string,object> h);
+
+		string[] ToStringArray ();
+
+		string[] ToStringArray (string property);
+
+		AbstractRecord[] ToArray ();
+
+		IRecordList Copy ();
+
+		bool TestAny (IRecordList irl);
+
 		bool Clean { get; set; }
-    }
+	}
 }
